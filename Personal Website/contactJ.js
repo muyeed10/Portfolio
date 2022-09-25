24135e1fd191592b28f851dc7d9c86c0e0a39a77
muyeed10 <83554763+muyeed10@users.noreply.github.com>
2022-09-25 03:08:07 -0400

    Add files via upload

---
+++ Personal Website/contactJ.js
@@ -0,0 +1,9 @@
+const contactForm = document.querySelector(".contactForm");
+
+const submitForm = document.querySelector(".submitContactForm");
+
+submitForm.addEventListener("click", (e)=> {
+    e.preventDefault();
+
+    //form text rules:
+});
