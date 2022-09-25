27f84bd1cec45a66c3e051978643d1b6d3c23875
muyeed10 <83554763+muyeed10@users.noreply.github.com>
2022-09-25 03:16:39 -0400

    Delete Personal Website directory

--- Personal Website/contactJ.js
+++
@@ -1,9 +0,0 @@
-const contactForm = document.querySelector(".contactForm");
-
-const submitForm = document.querySelector(".submitContactForm");
-
-submitForm.addEventListener("click", (e)=> {
-    e.preventDefault();
-
-    //form text rules:
-});
